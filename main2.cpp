9734b714e3fad1e1ebc71168a50fa5c8ae536fd5
nameissh <sehajeong@gmail.com>
2023-03-24 19:46:20 +0900

    Rock Scissors Paper_yellow sticker

--- main2.cpp
+++ main2.cpp
@@ -738,3 +738,70 @@ int main()
 	return 0;
 }*/
 
+// rockscissorspaper
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	VideoCapture cap(0);																					// camera_in()
+
+	if (!cap.isOpened())
+	{
+		cerr << "Camera open failed!" << endl;
+		return -1;
+	}
+
+	Scalar lowery = Scalar(20, 100, 100);																	// HSV color 노란색 범위 지정
+	Scalar uppery = Scalar(30, 255, 255);
+
+	Mat frame;
+	Mat hsv_frame;
+
+	while (true)
+	{
+		cap >> frame;																						// 실시간 영상 frame으로 받아오기
+		if (frame.empty())
+			break;
+
+		cvtColor(frame, hsv_frame, COLOR_BGR2HSV);
+
+		Mat yellow_mask, yellow_frame;
+		inRange(hsv_frame, lowery, uppery, yellow_mask);													// src_hsv 영상에서 HSV 색 성분 범위가 lowerb에서 upperb 사이인 위치의 픽셀만 흰색으로 설정한 mask 영상 생성
+
+
+		bitwise_and(frame, hsv_frame, yellow_frame, yellow_mask);											// frame + hsv_frame + yellow_mask = yellow_frame
+
+		Mat labels, stats, centroids;
+		int cnt = connectedComponentsWithStats(yellow_mask, labels, stats, centroids);						// 전체 레이블 개수로 return, cnt = labels - 1
+
+		int rock = 0;
+		int scissors = 0;
+		int paper = 0;
+		int num_yellow = cnt - 1;
+
+		String text;
+
+		if (num_yellow == 0)
+			text = "Rock!";
+
+		else if (num_yellow == 2)
+			text = "Scissors!";
+
+		else if (num_yellow == 5)
+			text = "Paper!";
+
+		putText(frame, text, Point(20, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
+
+		imshow("frame", frame);
+		imshow("yellow", yellow_frame);
+
+		if (waitKey(1) == 'q')
+			break;
+	}
+
+	return 0;
+}
